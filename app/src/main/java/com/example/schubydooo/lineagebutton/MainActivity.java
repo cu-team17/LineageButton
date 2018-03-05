2cdb018abe2b94b4b6d5cd06e77ce5fba1f2613a
schubydooo <scott.schubert@colorado.edu>
2018-03-04 17:07:48 -0700

    add init for gpio

--- app/src/main/java/com/example/schubydooo/lineagebutton/MainActivity.java
+++ app/src/main/java/com/example/schubydooo/lineagebutton/MainActivity.java
@@ -15,14 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Scott", "Starting shit");
+        Log.i("onCreate", "Starting shenanigans");
         try {
-            turnOnLED();
+            initGpio();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+    public void initGpio() throws  IOException {
+        Runtime r = Runtime.getRuntime();
+        Process p = null;
+
+        //gpio assignment
+        String redButton = "21";
+        String blueButton = "20";
+        String redLED = "6";
+        String blueLED = "5";
+
+        //Initialize red button
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  %s > /sys/class/gpio/export", redButton)});
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  in > /sys/class/gpio/gpio%s/direction", redButton)});
+
+        //Initialize blue button
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  %s > /sys/class/gpio/export", blueButton)});
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  in > /sys/class/gpio/gpio%s/direction", blueButton)});
+
+        //Initialize blue LED
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  %s > /sys/class/gpio/export", blueLED)});
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  out > /sys/class/gpio/gpio%s/direction", blueLED)});
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  1 > /sys/class/gpio/gpio%s/value", blueLED)});
+
+        //Initialize red LED
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  %s > /sys/class/gpio/export", redLED)});
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  out > /sys/class/gpio/gpio%s/direction", redLED)});
+        p = r.exec(new String[]{"/system/bin/su", "-c", String.format("echo  1 > /sys/class/gpio/gpio%s/value", redLED)});
+    }
+
 
     public void turnOnLED() throws IOException {
         Runtime r = Runtime.getRuntime();
